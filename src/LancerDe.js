b530e81cd5613ee8656e896e56655df988619e7f
NourKHD <78100628+NourKHD@users.noreply.github.com>
2022-01-01 18:53:36 +0100

    Update LancerDe.js

--- src/LancerDe.js
+++ src/LancerDe.js
@@ -11,27 +11,19 @@ class LancerDe extends React.Component{
         };
         this.lancer = this.lancer.bind(this);
     }
-         lancer = ()=>{ 
-            this.setState({
-            De1: Math.floor(Math.random() * 6) + 1,
-            De2: Math.floor(Math.random() * 6 ) + 1,
-        
-    })};
-    
-    
-
+    // fonction flèche qui modifie les stats de De1 et De2 avec un nbr aléatoire
+    lancer = ()=>{ 
+        this.setState({
+        De1: Math.floor(Math.random() * 6) + 1,
+        De2: Math.floor(Math.random() * 6 ) + 1,   
+        })
+   };
     render(){
         return(
             <div> 
+                //on appel le composant De avec les props fun, De1 et De2
                 <De fun={this.lancer} De1={this.state.De1} De2={this.state.De2}/>
-                {/* <h6>D1: {this.state.De1}</h6>
-                <h6>D2: {this.state.De2}</h6>
-                <h6>{this.state.De1img}</h6>
-                <h6>{this.state.De2img}</h6>
-                <h6>{this.props.func}</h6> */}
-                {/* <button onClick={this.onclick}>Lancer</button> */}
             </div>
-
         )
     }
 }
